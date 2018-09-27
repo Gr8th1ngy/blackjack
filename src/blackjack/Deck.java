5420faf4adcdb8ef14fbdbd9719e6881d242b870
Gr8th1ngy <anhhuyapmops@gmail.com>
2018-09-27 11:17:30 -0400

    Add Deck class and shuffle method for subclass Card

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,67 @@
+package blackjack;
+
+public class Deck {
+	private String suit[] = {"Heart","Spades","Diamonds","Clubs"};
+	private String value[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+	private Card deck[]= new Card[52];
+
+	public Deck() {
+		for (int i = 0; i < suit.length; i++) {
+			int j= 13 * i;
+			for (String value : value) {
+				deck[j]= new Card();
+				deck[j].setCardSuit(suit[i]);
+				deck[j].setCardValue(value);
+				j++;
+			}
+		}
+	}
+
+	public void shuffle() {
+		Card temp;
+		int a;
+		int b;
+
+		for (int j=0 ; j < 50; j++) {
+			a = (int) (Math.random()*52);
+			b = (int) (Math.random()*52);
+			temp = new Card (deck[a]);
+			deck[a] = new Card(deck[b]);
+			deck[b] = new Card(temp);
+		}
+	}
+
+	public Card[] getDeck() {
+		return deck;
+	}
+}
+
+class Card {
+
+	private String cardSuit;
+	private String cardValue;
+
+	public Card() {
+	}
+
+	public Card(Card card) {
+		this.cardSuit = card.getCardSuit();
+		this.cardValue = card.getCardValue();
+	}
+
+	public String getCardSuit() {
+		return cardSuit;
+	}
+
+	public void setCardSuit(String cardSuit) {
+		this.cardSuit = cardSuit;
+	}
+
+	public String getCardValue() {
+		return cardValue;
+	}
+
+	public void setCardValue(String cardValue) {
+		this.cardValue = cardValue;
+	}
+}
